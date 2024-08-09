ab79ca12dd0482805d1172685c659509094f0402
VICTOR EVANGELISTA CANDIDO DOS SANTOS <victor.evangelista@alunoescolassociais.org.br>
2024-08-09 07:17:09 -0300

    inserções de imagens como exemplo

--- controler.js
+++ controler.js
@@ -27,27 +27,26 @@ function salvarUser(){
         document.getElementById('nomeUser').value = "";    
     }
     else{
-        alert("Favor informar o nome");
+        alert("Informe algum nome !!!");
     }
 }
 
 function criaLista(){
 
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuario</th><th>Ações</th></tr>";
-    for(let i = 0; i <= (dadosLista.lenght - 1); i ++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> </td></tr>";
+    for(let i = 0; i <= (dadosLista.length - 1); i ++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> </td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
-
-    document.getElementById('tabela')
-    innerHTML = tabela;
 }
 
-function excluir(i){
-    dadosLista.splice((i - 1 ), 1);
-    document.getElementById('tabela').deleteRow(i);
-}
-
+// FUNÇÃO PARA EDITAR NOMES DE LISTA
 function editar(i){
-    Document.getElementById("nomeUser").value = dadosLista((i - 1)); dadosLista.splice(dadosLista[(i - 1)])
-
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
+}
+// FUNÇÃO PARA EXCLUIR NOME DE LISTA
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
 }
