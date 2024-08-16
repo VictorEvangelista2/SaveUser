0be72d327a01366e87a5a1b6bc5fcc703c8b77cf
VICTOR EVANGELISTA CANDIDO DOS SANTOS <victor.evangelista@alunoescolassociais.org.br>
2024-08-16 12:55:12 -0300

    .

--- controler.js
+++ controler.js
@@ -15,6 +15,7 @@ function acessar(){
 //função de criação de array para armazenamento de nomes
  
 var dadosLista = [];
+var emailLista = [];
 // Array criado para um valor indefinido
  
 function salvarUser(){
@@ -29,24 +30,42 @@ function salvarUser(){
     else{
         alert("Informe algum nome !!!");
     }
+
+    let emailUser = document.getElementById('emailUser').value;
+ 
+    if(emailUser){   
+        emailLista.push(emailUser);
+        console.log(emailLista);    
+        document.getElementById('emailUser').value="";  
+        criaLista();  
+    }
+    else{
+        alert("Informe algum Email Válido!!!");
+    }
 }
 
 function criaLista(){
 
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuario</th><th>Ações</th></tr>";
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuario</th><th>Email</th><th>Ações</th></tr>";
     for(let i = 0; i <= (dadosLista.length - 1); i ++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> </td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + emailLista[i] + "</td><td> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> </td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
+}
+
+
+// FUNÇÃO PARA EXCLUIR NOME DE LISTA
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    emailLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
 }
 
 // FUNÇÃO PARA EDITAR NOMES DE LISTA
 function editar(i){
     document.getElementById('nomeUser').value = dadosLista[(i - 1)];
     dadosLista.splice(dadosLista[(i - 1)], 1);
+    dadosLista.splice(emailLista[(i - 1)], 1);
 }
-// FUNÇÃO PARA EXCLUIR NOME DE LISTA
-function excluir(i){
-    dadosLista.splice((i - 1), 1);
-    document.getElementById('tabela').deleteRow(i);
-}
+
+
